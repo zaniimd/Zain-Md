862724dedd6ddf66357647ab23844c96f2171ab1
zaniimd <zainkhosa968@gmail.com>
2025-11-15 01:54:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",  // ADD YOUR SESSION ID 
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/atby2t.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that your name is ALIVE now—ready to dive into new adventures with you!",
-SUDO_NB: process.env.SUDO_NB || "your number",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/lkp2vn.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that Zain Khosa is ALIVE now—ready to dive into new adventures with you!",
+SUDO_NB: process.env.SUDO_NB || "923104321993",
 AUTO_REACT: process.env.AUTO_REACT || "true", // Enable/disable auto reaction (true/false)
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"false",
 OMDB_API_KEY: "5e339fb7",
-OWNER_NAME: process.env.OWNER_NAME || "your name",
+OWNER_NAME: process.env.OWNER_NAME || "ZAIN KHOSA",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true"
 };
